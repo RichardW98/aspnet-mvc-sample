db3c4d329d57a9a2d88959a9ffef3d44f8746b3b
RichardW98 <richwong98@gmail.com>
2021-02-21 16:06:18 -0500

    aspnet mvc

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Collections;
+
+namespace Sample.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        public string Getdb() {
+            //try
+            //{
+            //    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            //    builder.DataSource = "<your_server>.database.windows.net";
+            //    builder.UserID = "<your_username>";
+            //    builder.Password = "<your_password>";
+            //    builder.InitialCatalog = "<your_database>";
+
+            //    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            //    {
+            //        Console.WriteLine("\nQuery data example:");
+            //        Console.WriteLine("=========================================\n");
+
+            //        String sql = "SELECT name, collation_name FROM sys.databases";
+
+            //        using (SqlCommand command = new SqlCommand(sql, connection))
+            //        {
+            //            connection.Open();
+            //            using (SqlDataReader reader = command.ExecuteReader())
+            //            {
+            //                while (reader.Read())
+            //                {
+            //                    Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //}
+            //Console.ReadLine();
+            string resp = "";
+            foreach (DictionaryEntry variable in Environment.GetEnvironmentVariables())
+                resp += variable.Key + ": " + variable.Value + ", ";
+            return resp;
+        }
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,9 @@
-FROM mcr.microsoft.com/dotnet/framework/sdk:4.8 AS build
-ENV FOO=bar
-WORKDIR /app
+#Depending on the operating system of the host machines(s) that will build or run the containers, the image specified in the FROM statement may need to be changed.
+#For more information, please see https://aka.ms/containercompat 
 
-# copy csproj and restore as distinct layers
-COPY *.sln .
-COPY aspnetmvcapp/*.csproj ./aspnetmvcapp/
-COPY aspnetmvcapp/*.config ./aspnetmvcapp/
-RUN nuget restore
-
-# copy everything else and build app
-COPY aspnetmvcapp/. ./aspnetmvcapp/
-WORKDIR /app/aspnetmvcapp
-RUN msbuild /p:Configuration=Release -r:False
-
-
-FROM mcr.microsoft.com/dotnet/framework/aspnet:4.8 AS runtime
-ENV FOO=bar
+FROM mcr.microsoft.com/dotnet/framework/aspnet:4.7.2-windowsservercore-ltsc2019
+ENV FOO="far"
 WORKDIR /inetpub/wwwroot
-COPY --from=build /app/aspnetmvcapp/. ./
+COPY obj/Release/Package/PackageTmp/ .
+EXPOSE 80
+ENV ASPNETCORE_URLS=http://*:80
